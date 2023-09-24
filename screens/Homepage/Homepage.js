8a14bde59d6233f894ab8a635a8994c8e75d0a39
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2023-09-24 21:11:42 +0530

    adding homepage

---
+++ screens/Homepage/Homepage.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Homepage</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Homepage
+
+const styles = StyleSheet.create({})
