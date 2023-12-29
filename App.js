1a7a4314ae433580861e9b9481023383b83d7139
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2023-12-29 23:02:32 +0530

    have suppressed the log

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, LogBox } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import StackNavigator from "./StackNavigation";
 import store from "./store";
+
+LogBox.ignoreLogs(["AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage"]);
 
 export default function App() {
   return (
